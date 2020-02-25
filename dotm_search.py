dc2b92bc975a83eb28c0c22b3b35c49cbde55064
luisfff29 <luisfff29@gmail.com>
2020-02-24 18:23:53 -0600

    update author

--- dotm_search.py
+++ dotm_search.py
@@ -5,7 +5,7 @@
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "luisfff29"
 
 
 def main():
