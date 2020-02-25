6d2f9fc8c822dbcc21b4f261866ee72b500d1316
luisfff29 <luisfff29@gmail.com>
2020-02-25 12:51:38 -0600

    import os

--- dotm_search.py
+++ dotm_search.py
@@ -8,9 +8,11 @@ within the 'word/document.xml' section of a MSWord .dotm file.
 __author__ = "luisfff29"
 
 import zipfile
+import os
 
 with zipfile.ZipFile('dotm_files/@999.dotm') as zf:
     print(zf.read('word/document.xml'))
+print(os.getcwd)
 
 
 def main():
