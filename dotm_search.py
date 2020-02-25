8fb9885126a706eb47f263424ad169c8c5eca1ad
luisfff29 <luisfff29@gmail.com>
2020-02-25 13:06:31 -0600

    create_parser / argparse

--- dotm_search.py
+++ dotm_search.py
@@ -7,12 +7,23 @@ within the 'word/document.xml' section of a MSWord .dotm file.
 """
 __author__ = "luisfff29"
 
+import argparse
 import zipfile
 import os
+
 
 with zipfile.ZipFile('dotm_files/@999.dotm') as zf:
     print(zf.read('word/document.xml'))
 print(os.getcwd)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Print full path name of each file and a partial line of the dotm text that was found to contain the search text.")
+    parser.add_argument('--dir', action="store",
+                        help="OPTIONAL directory of .dotm files to scan")
+    parser.add_argument('text', help="text to search for", required=True)
+    return parser
 
 
 def main():
